3e49136a8686f955a39833c95ba78d808215e8d5
Joselu <joseluissgz97@gmail.com>
2025-01-25 12:53:53 +0100

    feat: add currentPath to show where the user is located and renering NAV_LINKS

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+  { href: '/about-us', text: 'About us' },
+  { href: '/rooms', text: 'Rooms' },
+  { href: '/offers', text: 'Offers' },
+  { href: '/contact', text: 'Contact' }
+];
